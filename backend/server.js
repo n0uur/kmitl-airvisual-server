5772a166cab03dcef66d43daecf158459df77a28
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2024-12-03 22:48:10 +0700

    feat: use bun as it should

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,48 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const cron = require('node-cron');
-const Data = require('./models/Data');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cron = require("node-cron");
+const Data = require("./models/Data");
 
 const app = express();
 const port = 3100;
 const apiUrl = `http://api.airvisual.com/v2/nearest_city?lat=13.721434635446425&lon=100.78113540955499&key=${process.env.API_KEY}`;
 
 // MongoDB Connection URI using environment variables
-const dbURI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-console.log(dbURI)
+const dbURI = process.env.DB_CONNECTION_STRING;
+console.log(dbURI);
 // Connect to MongoDB
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('Failed to connect to MongoDB:', err));
+mongoose
+  .connect(dbURI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
 // Function to fetch and update data every 10 minutes
 const fetchAndUpdateData = async () => {
-    try {
-        const response = await axios.get(apiUrl);
-        const data = response.data.data;
+  try {
+    const response = await axios.get(apiUrl);
+    const data = response.data.data;
 
-        const newData = {
-            city: data.city,
-            state: data.state,
-            country: data.country,
-            location: data.location,
-            pollution: data.current.pollution,
-            weather: data.current.weather,
-        };
+    const newData = {
+      city: data.city,
+      state: data.state,
+      country: data.country,
+      location: data.location,
+      pollution: data.current.pollution,
+      weather: data.current.weather,
+    };
 
-        // Replace the existing data in the database
-        await Data.findOneAndReplace({}, newData, {upsert: true});
-        console.log('Data updated successfully');
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+    // Replace the existing data in the database
+    await Data.findOneAndReplace({}, newData, { upsert: true });
+    console.log("Data updated successfully");
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
 // Fetch and update data every 10 minutes
-cron.schedule('*/10 * * * *', fetchAndUpdateData);
+cron.schedule("*/10 * * * *", fetchAndUpdateData);
 
 // Initial fetch on server start
 fetchAndUpdateData();
@@ -51,18 +51,18 @@ fetchAndUpdateData();
 app.use(cors());
 
 // Route to get stored data
-app.get('/', async (req, res) => {
-    try {
-        const data = await Data.findOne({});
-        if (!data) {
-            return res.status(404).json({message: 'No data found'});
-        }
-        res.json(data);
-    } catch (error) {
-        res.status(500).json({message: 'Error fetching data', error});
+app.get("/", async (req, res) => {
+  try {
+    const data = await Data.findOne({});
+    if (!data) {
+      return res.status(404).json({ message: "No data found" });
     }
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching data", error });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
